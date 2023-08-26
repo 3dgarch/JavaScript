db413db26a353f2697bd5d7577b0bd9a347b08d7
3dgarch <edgarchavezg93@gmail.com>
2023-08-26 10:54:11 -0500

    Mdulos import/export

---
+++ 31-Modulos-import-export/js/constantes.js
@@ -0,0 +1,9 @@
+export const PI = Math.PI;
+
+const pass = "qwerty"
+//export default pass;
+
+export default function saludar(){
+  console.log("Hola modulos +ES6")
+}
+
---
+++ 31-Modulos-import-export/js/main.js
@@ -0,0 +1,16 @@
+
+//console.log("Archivos modulos");
+import saludar, {PI} from "./constantes.js";
+
+//import {sumar,restar} from "./aritmetica.js"
+import {aritmetica as ari} from "./aritmetica.js"
+
+
+console.log(PI)
+
+//console.log(sumar(3,4))
+console.log(ari.restar(18,5))
+
+//console.log(pass)
+
+saludar();
