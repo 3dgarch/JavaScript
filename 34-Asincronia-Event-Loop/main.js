380326515e5256520365a63b1fccd4f96cfefb48
3dgarch <edgarchavezg93@gmail.com>
2023-09-27 10:17:56 -0500

    Asincronia y event loop

--- 34-Asincronia-Event-Loop/main.js
+++ 34-Asincronia-Event-Loop/main.js
@@ -30,5 +30,22 @@
 
 /* Codigo Asincrono No Bloqueante*/
 (()=>{
-  
+  console.log("Codigo Asincrono");
+  console.log("Inicio");
+  function dos(){
+    setTimeout(function(){
+      console.log("Dos")
+    },1000);
+  }
+
+  function uno(){
+    setTimeout(function(){
+      console.log("Uno");
+    },0);
+    dos();
+    console.log("Tres")
+  }
+
+  uno();
+  console.log("Fin")
 })()
